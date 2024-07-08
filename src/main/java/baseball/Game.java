6d07c15d16197192a30e0fd5ec4c5b160b1fa0fa
taekyunlee <dlxorbs0219@gmail.com>
2024-07-08 15:54:37 +0900

    feat : newGame() 함수 생성

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -9,20 +9,25 @@ public class Game {
         this.input = new Input();
         this.go = new Go();
     }
+    public void newGame(){
+        while(true){
+            System.out.println("숫자를 입력해주세요 : ");
+            int[] player = input.getInput();
+            String result = go.Check(computer.getCom(), player);
+            System.out.println(result);
+
+            if(result.equals("3스트라이크")){
+                break;
+            }
+        }
+    }
     public void start(){
         while(true){
             System.out.println("숫자 야구 게임을 시작합니다.");
             computer.Random();
-            while(true){
-                System.out.println("숫자를 입력해주세요 : ");
-                int[] player = input.getInput();
-                String result = go.Check(computer.getCom(), player);
-                System.out.println(result);
 
-                if(result.equals("3스트라이크")){
-                    break;
-                }
-            }
+            newGame();
+
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             String next = input.getUserInput();
